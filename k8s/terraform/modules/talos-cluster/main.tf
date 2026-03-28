6bf98c8f9841107a3867dc98d07ae9174c5901fe
venndev20999 <khoapham.dev20999@gmail.com>
2026-03-28 14:52:29 +0700

    cilium init

--- k8s/terraform/modules/talos-cluster/main.tf
+++ k8s/terraform/modules/talos-cluster/main.tf
@@ -119,8 +119,8 @@ resource "helm_release" "cilium" {
       kubeProxyReplacement = true
       securityContext = {
         capabilities = {
-          ciliumAgent      = "{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}"
-          cleanCiliumState = "{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}"
+          ciliumAgent      = ["CHOWN", "KILL", "NET_ADMIN", "NET_RAW", "IPC_LOCK", "SYS_ADMIN", "SYS_RESOURCE", "DAC_OVERRIDE", "FOWNER", "SETGID", "SETUID"]
+          cleanCiliumState = ["NET_ADMIN", "SYS_ADMIN", "SYS_RESOURCE"]
         }
       }
       cgroup = {
