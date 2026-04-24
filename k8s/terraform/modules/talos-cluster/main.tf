293df772aa48821ca8e54fd893ebb141382e49b7
venndev20999 <khoapham.dev20999@gmail.com>
2026-04-25 00:04:57 +0700

    Restore ingressController for RBAC

--- k8s/terraform/modules/talos-cluster/main.tf
+++ k8s/terraform/modules/talos-cluster/main.tf
@@ -192,6 +192,12 @@ resource "helm_release" "cilium" {
           enabled = true
         }
       }
+      ingressController = {
+        enabled = true
+        loadbalancer = {
+          annotation = true
+        }
+      }
       l2announcements = {
         enabled = true
       }
