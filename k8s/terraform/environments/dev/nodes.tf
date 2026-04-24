55d718aecfd0d53a95696f9f230437af33cba277
venndev20999 <khoapham.dev20999@gmail.com>
2026-04-24 22:30:27 +0700

    Update config

--- k8s/terraform/environments/dev/nodes.tf
+++ k8s/terraform/environments/dev/nodes.tf
@@ -6,11 +6,11 @@
 # Let's define the nodes data as a local variable for clarity.
 locals {
   # Prefix to be used for node names, similar to Ansible's talos_prefix
-  node_prefix = "dev" # You might want to get this from a variable
+  node_prefix = "dev"
 
   talos_masters = [
     {
-      name      = "talos-vm-master-1-${local.node_prefix}"
+      name      = "tl-master-${local.node_prefix}"
       ip        = "192.168.122.20"
       memory    = 4096
       vcpus     = 4
@@ -20,63 +20,39 @@ locals {
 
   talos_workers = [
     {
-      name      = "talos-vm-worker-1-${local.node_prefix}"
+      name      = "tl-worker-1-${local.node_prefix}"
       ip        = "192.168.122.21"
       memory    = 2048
       vcpus     = 2
       disk_size = 30
     },
     {
-      name      = "talos-vm-worker-2-${local.node_prefix}"
+      name      = "tl-worker-2-${local.node_prefix}"
       ip        = "192.168.122.22"
       memory    = 2048
       vcpus     = 2
       disk_size = 30
     },
     {
-      name      = "talos-vm-worker-3-${local.node_prefix}"
+      name      = "tl-worker-3-${local.node_prefix}"
       ip        = "192.168.122.23"
       memory    = 2048
       vcpus     = 2
       disk_size = 30
     },
     {
-      name      = "talos-vm-worker-4-${local.node_prefix}"
+      name      = "tl-worker-4-${local.node_prefix}"
       ip        = "192.168.122.24"
+      memory    = 2048
+      vcpus     = 2
+      disk_size = 30
+    },
+    {
+      name      = "tl-worker-5-${local.node_prefix}"
+      ip        = "192.168.122.25"
       memory    = 2048
       vcpus     = 2
       disk_size = 30
     }
   ]
 }
-
-variable "talos_iso_path" {
-  type    = string
-  default = "/var/iso/metal-amd64.iso"
-}
-
-# module "masters" {
-#   source = "git::ssh://git@github.com/venndev20999/homelabs-infrastructure-module.git//terraform/talos-instance"
-# 
-#   for_each = { for n in local.talos_masters : n.name => n }
-# 
-#   name       = each.value.name
-#   ip_address = each.value.ip
-#   memory     = each.value.memory
-#   vcpus      = each.value.vcpus
-#   disk_size  = each.value.disk_size
-#   iso_path   = var.talos_iso_path
-# }
-# 
-# module "workers" {
-#   source = "git::ssh://git@github.com/venndev20999/homelabs-infrastructure-module.git//terraform/talos-instance"
-# 
-#   for_each = { for n in local.talos_workers : n.name => n }
-# 
-#   name       = each.value.name
-#   ip_address = each.value.ip
-#   memory     = each.value.memory
-#   vcpus      = each.value.vcpus
-#   disk_size  = each.value.disk_size
-#   iso_path   = var.talos_iso_path
-# }
