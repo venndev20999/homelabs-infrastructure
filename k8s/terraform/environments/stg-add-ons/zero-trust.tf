79cecb7b9a9259e0613287d949fad4d2ec9e3c6d
venndev20999 <khoapham.dev20999@gmail.com>
2026-08-18 11:13:06 +0700

    tf: add tailscale allow for home network

--- k8s/terraform/environments/stg-add-ons/zero-trust.tf
+++ k8s/terraform/environments/stg-add-ons/zero-trust.tf
@@ -6,6 +6,7 @@ module "tailscale" {
   tailscale_auth_key = var.tailscale_auth_key
   advertise_routes = [
     "192.168.122.0/24", // External VM NAT/Staging subnet
+    "192.168.1.0/24",   // Home subnet
     "10.244.0.0/16",    // Kubernetes Pod CIDR
     "10.96.0.0/12"      // Kubernetes Service CIDR
   ]
